b42bf3dbe29c8749f6d728f261fb745e5037760d
Phil Crump <phil@philcrump.co.uk>
2016-01-31 03:15:31 +0000

    Improve detection of disconnection.

--- screens_goonhilly.sh
+++ screens_goonhilly.sh
@@ -10,9 +10,9 @@ sudo killall fbi;
 sudo fbi -T 1 -noverbose -a Black_Goonhilly_Standingby.png;
 
 while true; do
-  sleep 1;
+  sleep 3;
   # Check network connection
-  if ! ifconfig | grep -F "10.155.0." > /dev/null; then
+  if ! ping -c 1 -W 3 10.155.0.1 > /dev/null; then
     sudo killall fbi;
     # Wait for network
     sudo fbi -T 1 -noverbose -a Black_Goonhilly_Connecting.png;
--- screens_landrover.sh
+++ screens_landrover.sh
@@ -12,7 +12,7 @@ sudo fbi -T 1 -noverbose -a Black_Landrover_StandingBy.png;
 while true; do
   sleep 1;
   # Check network connection
-  if ! ifconfig | grep -F "10.155.0." > /dev/null; then
+  if ! ping -c 1 -W 3 10.155.0.1 > /dev/null; then
     sudo killall fbi;
     # Wait for network
     sudo fbi -T 1 -noverbose -a Black_Landrover_Connecting.png;
