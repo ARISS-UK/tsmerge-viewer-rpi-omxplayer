d99d6c5f6e63d5ccdb4f93a6acd8aadf410bad09
Phil Crump <phil@philcrump.co.uk>
2016-02-11 10:27:25 +0000

    Remove subtitles.

--- rx_goonhilly.sh
+++ rx_goonhilly.sh
@@ -5,6 +5,6 @@ while true; do
   mkfifo fifostream;
   sudo killall omxplayer;
   sudo killall omxplayer.bin;
-  omxplayer --live -o hdmi --subtitles goonhilly.srt --align center --audio_fifo 0.2 --video_fifo 0.2 --video_queue 0.2 --threshold 0.5 --timeout 10 --aspect-mode stretch --win 0,0,1920,1080 --adev local fifostream &
+  omxplayer --live -o hdmi --align center --audio_fifo 0.2 --video_fifo 0.2 --video_queue 0.2 --threshold 0.5 --timeout 10 --aspect-mode stretch --win 0,0,1920,1080 --adev local fifostream &
   nc -p 1234 -l -w 10 > fifostream;
 done;
